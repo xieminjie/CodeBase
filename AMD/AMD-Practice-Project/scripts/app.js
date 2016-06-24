9c73e61d06c8996fcba6942d294f88ae63be9093
XieMinjie <xieminjie@XieMinjies-MacBook-Pro.local>
2016-06-24 11:33:03 +1000

    set up file structure

--- AMD/AMD-Practice-Project/scripts/app.js
+++ AMD/AMD-Practice-Project/scripts/app.js
@@ -1,7 +1,4 @@
 define(['jquery'], function($) {    
-	// We use the object defined in the 'messages' module and passed to our
-	// callback by RequireJS and call its 'getHello'() function when the
-	// DOM has been loaded using the jQuery module
 	$(function() {
 		$('#test').text('helloworld');
 	});    
